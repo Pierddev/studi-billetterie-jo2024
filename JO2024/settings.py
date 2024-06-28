0811c34a7ad0d8b276641cd3348c1ba0bb7ab8f6
Pierddev <pierre.duault.pro@gmail.com>
2024-06-28 20:49:03 +0200

    heroku Server Error (500)

--- JO2024/settings.py
+++ JO2024/settings.py
@@ -170,13 +170,13 @@ USE_TZ = True
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATIC_URL = '/static/'
 
-STORAGES = {
-    # Enable WhiteNoise's GZip and Brotli compression of static assets:
-    # https://whitenoise.readthedocs.io/en/latest/django.html#add-compression-and-caching-support
-    "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
-    },
-}
+# STORAGES = {
+#     # Enable WhiteNoise's GZip and Brotli compression of static assets:
+#     # https://whitenoise.readthedocs.io/en/latest/django.html#add-compression-and-caching-support
+#     "staticfiles": {
+#         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+#     },
+# }
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
